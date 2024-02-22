986c0f86b327362e7ae0787c8f4cc9379f924500
Alexandranuthu <alexandranuthuu@gmail.com>
2024-02-22 14:27:03 +0300

    made changes to contact

--- app/Models/Contacts.php
+++ app/Models/Contacts.php
@@ -1,11 +1,26 @@
 <?php
 
 namespace App\Models;
-
+use App\Models\Organisation;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Contacts extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'email',
+        'phone',
+        'job_title',
+        'organization_id',
+        'image'
+    ];
+    //Add relationships
+    public function organization()
+    {
+        return $this->belongsTo(Organisation::class);
+    }
 }
